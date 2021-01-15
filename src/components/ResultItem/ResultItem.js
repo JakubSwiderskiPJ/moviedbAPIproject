23a4edad86027b7f5c82abcc63c992f3f8d150aa
JakubSwiderskiPJ <s19443@pjwstk.edu.pl>
2021-01-15 23:49:26 +0100

    API and search component

---
+++ src/components/ResultItem/ResultItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ResultItem.scss';
+
+export default function ResultItem() {
+
+
+    return (
+        <li>
+
+        </li>
+    );
+}
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.scss';
+import ResultItem from "../ResultItem/ResultItem";
 
 export default function SearchBar() {
 
+    const [result, setResult] = useState([]);
 
+    
 
     return (
-        <form>
-            <label htmlFor="search-bar">Znajdź Swój Film...</label>
-            <input id="search-bar" type="text"/>
-        </form>
+        <article className='search-film-bar'>
+            <form className='search-film-form'>
+                <label htmlFor="search-bar" />
+                <input id="search-bar"
+                       className='search-film-input search-film-input-justification'
+                       type="text"
+                       placeholder='Wpisz tytuł filmu..' />
+            </form>
+            <ul className="search-result-list">
+                <ResultItem />
+            </ul>
+        </article>
     );
 };
